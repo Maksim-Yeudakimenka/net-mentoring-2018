4de537794f7c3dd7b5126a8c22f8860cc920a495
Max Evdokimenko <evdokimenko.maxim@gmail.com>
2018-03-17 21:46:36 +0300

    Multithreading Task 1

---
+++ 01. Multi-Threading in .NET/Multithreading/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var tasks = new Task[100];
+
+      for (var i = 0; i < 100; i++)
+      {
+        var j = i;
+        tasks[i] = new Task(() => IterateAndPrint(j));
+        tasks[i].Start();
+      }
+
+      Task.WaitAll(tasks);
+    }
+
+    static void IterateAndPrint(int taskIndex)
+    {
+      for (var k = 0; k < 1000; k++)
+      {
+        Console.WriteLine($"Task #{taskIndex} – {k}");
+      }
+    }
+  }
+}
