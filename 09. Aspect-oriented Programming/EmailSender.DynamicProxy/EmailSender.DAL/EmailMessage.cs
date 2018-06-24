230bebc428d9efee7a6864da3fa5d695094c1331
Max Evdokimenko <evdokimenko.maxim@gmail.com>
2018-06-25 00:10:37 +0300

    Copy of EmailSender solution

---
+++ 09. Aspect-oriented Programming/EmailSender.DynamicProxy/EmailSender.DAL/EmailMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace EmailSender.DAL
+{
+  public class EmailMessage
+  {
+    public int Id { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+    public string Recipient { get; set; }
+    public bool Completed { get; set; }
+  }
+}
